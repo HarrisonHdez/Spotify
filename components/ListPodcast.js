b6db3574147c2106d7596d57b4a2e6cdf35b9278
Harrison Hdez <hhdezprogrammer@gmail.com>
2023-07-06 20:21:24 -0600

    first commit

---
+++ components/ListPodcast.js
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import React from "react";
+import Podcast from "./Podcast";
+import Podcast2 from "./Podcast2";
+
+const ListPodcast = (props) => {
+  const { title } = props;
+
+  return (
+    <div className="mb-8">
+      <div className="flex items-center justify-between mb-8">
+        <Link
+          href="/"
+          className="text-2xl font-bold text-white hover:underline"
+        >
+          {title}
+        </Link>
+        <Link
+          href="/"
+          className="uppercase text-xs font-bold tracking-[2px] hover:underline"
+        >
+          Mostrar todo
+        </Link>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 gap-6">
+        <Podcast />
+        <Podcast2 />
+        <Podcast />
+        <Podcast />
+        <Podcast />
+        <Podcast />
+        
+      </div>
+    </div>
+  );
+};
+
+export default ListPodcast;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import ListAlbums from "../components/ListAlbums";
+import ListPodcast from "../components/ListPodcast";
 
 export default function Home() {
   return (
     <div className="pt-28 md:pl-72 p-8">
       <ListAlbums title="Spotify Playlists" />
+      <ListPodcast title="Pódcast" />
       <ListAlbums title="Enfoque" />
-      <ListAlbums title="Pódcast" />
-      <ListAlbums title="Estado de ánimo" />
+      <ListAlbums title="Listas destacadas" />
     </div>
   );
 }
